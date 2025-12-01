15842d16bac6454410b84d3d79fd8cdbc68b9b3a
Pavel Skripkin <paskripkin@gmail.com>
2025-12-01 19:25:52 +0300

    remove extra memory allocation for fdt

--- kernel/src/drivers/fdt.rs
+++ kernel/src/drivers/fdt.rs
@@ -1,8 +1,4 @@
-use crate::mm::allocators::page_alloc::page_allocator;
-use hal::{
-    address::{LinearAddr, PhysAddr, VirtAddr},
-    arch::PAGE_SIZE,
-};
+use hal::address::{LinearAddr, PhysAddr};
 use loader_protocol::LoaderArg;
 use spin::Once;
 
@@ -14,23 +10,8 @@ pub struct Fdt {
 pub static FDT: Once<Fdt> = Once::new();
 
 pub fn init(arg: &LoaderArg) {
-    let total_pages = arg.fdt_size.next_multiple_of(PAGE_SIZE) / PAGE_SIZE;
-    let pages = page_allocator()
-        .alloc(total_pages)
-        .expect("Failed to allocate memory for FDT");
-
-    let va_from: VirtAddr = LinearAddr::from(PhysAddr::new(arg.fdt_base)).into();
-    let linear: LinearAddr = pages.into();
-    let mut va_to: VirtAddr = linear.into();
-
-    unsafe {
-        va_to
-            .as_slice_mut::<u8>(arg.fdt_size)
-            .copy_from_slice(va_from.as_slice::<u8>(arg.fdt_size));
-    }
-
     FDT.call_once(|| Fdt {
-        base: linear,
+        base: LinearAddr::from(PhysAddr::new(arg.fdt_base)),
         size: arg.fdt_size,
     });
 }
