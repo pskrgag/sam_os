1a37cb59bc68ba59cd58e12230890d2fd86f0bf5
Pavel Skripkin <paskripkin@gmail.com>
2026-07-25 22:45:10 +0300

    add backslash in console

--- userspace/apps/console/src/console.rs
+++ userspace/apps/console/src/console.rs
@@ -3,6 +3,7 @@ use super::bindings_Vfs::Vfs;
 use crate::commands::{commands, Enviroment};
 use crate::cwd::Cwd;
 use alloc::{string::String, vec::Vec};
+use heapless::String as HLString;
 
 pub struct Console {
     backend: Serial,
@@ -21,20 +22,33 @@ impl Console {
             .unwrap();
     }
 
+    async fn put_byte(&self, byte: u8) {
+        let mut s = HLString::new();
+
+        // this will never happen
+        s.push(byte as char).unwrap();
+        self.backend.Put(s.try_into().unwrap()).await.unwrap();
+    }
+
     async fn read_until_newline(&self) -> String {
         let mut res = String::new();
 
         loop {
             let new = self.backend.GetByte().await.unwrap();
 
-            if new.byte == b'\r' {
-                self.put_str("\n").await;
-                break res;
-            } else {
-                let mut s = String::with_capacity(1);
-
-                s.push(new.byte as char);
-                self.put_str(&s).await;
+            match new.byte {
+                b'\r' => {
+                    self.put_str("\n").await;
+                    break res;
+                }
+                0x08 | 0x7f => {
+                    res.pop();
+                    self.put_str("\x08 \x08").await;
+                    continue;
+                }
+                s => {
+                    self.put_byte(s).await;
+                }
             }
 
             res.push(new.byte as char);
@@ -45,8 +59,8 @@ impl Console {
         let mut cwd = Cwd::root(&self.vfs).await.unwrap();
 
         loop {
-            self.put_str(cwd.name()).await;
-            self.put_str(" > ").await;
+            self.put_str(alloc::format!("{} > ", cwd.name())).await;
+
             let cmd = self.read_until_newline().await;
             if cmd.is_empty() {
                 continue;
